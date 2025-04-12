f820bd48deeb7f20aa700561550ba04487f8c2db
VijayJust <vijaysundar92301@gmail.com>
2025-04-12 21:29:32 +0530

    Update Myntra.java

--- Myntra.java
+++ Myntra.java
@@ -3,5 +3,7 @@ public class Myntra
     public static void (String [] args)
     {
        system.out.println("Order placed") 
+       system.out.println("order arrived")
+      system.out.println("order deliveryed")
     }
   }
